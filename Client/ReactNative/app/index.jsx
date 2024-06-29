997906e70c1d9eabbaf1303a40cea5983d98b4ed
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-06-30 01:01:26 +0700

    ui login

--- Client/ReactNative/app/index.jsx
+++ Client/ReactNative/app/index.jsx
@@ -3,9 +3,18 @@ import React from "react";
 import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import AsyncStorage from "@react-native-async-storage/async-storage"
+const getToken = async()=>{
+  try {
+    const token =await AsyncStorage.getItem("AccessToken")
+    return token
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 const Welcome = () => {
-  return <Redirect href="/home" />;
+  return <Redirect href="/sign-in" />;
 };
 
 export default Welcome;
--- Server/app.js
+++ Server/app.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+
+const cors = require('cors');
+app.use(cors());
 //routes
 const routerAuth = require('./routes/auth.routes');
 const routeRole = require('./routes/role.routes')
