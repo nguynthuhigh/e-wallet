d89622a8b1ee347c0519afa4f67b40bdca163adf
truongdinhtrung <truongdinhtrungob@gmail.com>
2024-06-30 19:27:55 +0700

    update

--- Client/ReactNative/app/(tabs)/transaction-history/_layout.jsx
+++ Client/ReactNative/app/(tabs)/transaction-history/_layout.jsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 const StackLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index"  options={{headerShown:false}}/>
     </Stack>
   );
 };
