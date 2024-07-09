3b36ac38518a9b7da0144e6b3a3a3359186230fa
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-07-10 01:34:55 +0700

    pull

--- Client/ReactNative/app/(tabs)/home/confirm-send.jsx
+++ Client/ReactNative/app/(tabs)/home/confirm-send.jsx
@@ -5,31 +5,64 @@ import {
   TextInput,
   TouchableOpacity,
   ScrollView,
-  SafeAreaView
+  SafeAreaView,
+  StyleSheet
 } from "react-native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { Message } from "../../../dummy-data/data.js";
 import BackArrow from "../../../assets/svg/arrow_back.svg";
 import BlueBg from "../../../assets/svg/bg_blue.svg";
-import { Link, router } from "expo-router";
-
+import { useLocalSearchParams,Link, router } from "expo-router";
+import walletAPI from '../../api/wallet.api.js'
+import Modal from 'react-native-modal'
 const Wallet = ()=>{
   return(
-    <View>
-          
-    </View>
+    <TouchableOpacity className="w-[30%] h-[55px] bg-[#FFF5F5] rounded-xl flex-row items-center">
+          <Image className="w-[20px] h-[20px] m-2"  source={{
+                uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNFPx_5UeN24Si_c-S11x4Xq0T10ydstaLig&s',
+            }}></Image>
+          <View>
+              <Text className='text-[16px] font-semibold '>
+                  VND
+              </Text>
+              <Text className='text-[12px]  '>200000</Text>
+          </View>
+    </TouchableOpacity>
   )
 }
-
 const ConfirmSend = () => {
-  const [price, setPrice] = useState(0);
+  const params = useLocalSearchParams();
+  const recevier = params.receiver
+  const [amount, setAmount] = useState(0);
   const [content, setContent] = useState("");
+  const [isLoading,setIsLoading] = useState(true)
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+  useEffect(()=>{
+    const fetchWallet = ()=>{
+      const responese = walletAPI.getWallet();
+    }
+  },[])
+
+  const handleTransfer =async()=>{
+    const data = {
+      receiver: recevier,
+      amount: amount,
+      message: content,
+      currency: "VND"
+    }
+    // const response = await walletAPI.sendMoney(data)
+    toggleModal()
+  }
   return (
     <SafeAreaView>
       <BlueBg />
       <SafeAreaView className="absolute top-7">
+       
         <View className="flex-row items-center px-5 gap-x-2 mb-4">
           <TouchableOpacity onPress={() => router.back()}>
             <BackArrow width={30} height={26.5} />
@@ -47,15 +80,38 @@ const ConfirmSend = () => {
           </View>
         </View>
         <ScrollView className="px-5">
+        <Modal isVisible={isModalVisible} className="">
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}} >
 
+            <View className="bg-[#FAFAFA] font-semibold text-[22px] w-full p-4 rounded-t-[10px]">
+              <TouchableOpacity onPress={()=>{setModalVisible(!isModalVisible)}}><Text>close</Text></TouchableOpacity>
+              <Text className="font-semibold text-[22px] text-center">Nhập mã bảo mật</Text>
+              </View>
+            <View className="bg-[#EEEEEE] w-full flex-row justify-center py-4 rounded-b-lg">
+              <TextInput  
+                style={styles.input} 
+                className="font-semibold w-fit bg-white rounded-full px-5" 
+                placeholder='••••••'
+                maxLength = {6}
+                keyboardType='numeric'
+                autoFocus={true}
+                >
+                </TextInput>
+            </View>
+         </View>
+        </Modal>
           <View className="flex-1 mx-auto bg-white rounded-xl shadow-2xl">
-            <Wallet></Wallet>
+            <View className="flex-row justify-between my-5 px-4">
+              <Wallet></Wallet>
+              <Wallet></Wallet>
+              <Wallet></Wallet>
+            </View>
             <View className="mx-auto my-5">
               <TextInput
-                onChangeText={(newText) => setPrice(newText)}
+                onChangeText={(newText) => setAmount(newText)}
                 keyboardType={"number-pad"}
                 autoFocus={true}
-                value={price}
+                value={amount}
                 className="w-fit h-[50px] text-[50px] font-bold"
                 placeholder="0đ"
               ></TextInput>
@@ -107,11 +163,11 @@ const ConfirmSend = () => {
               </View>
             </TouchableOpacity>
           </View>
-          <View className="bg-[#0D99FF] rounded-xl p-2 mt-4">
+          <TouchableOpacity onPress={handleTransfer} className="bg-[#0D99FF] rounded-xl p-2 mt-4">
             <Text className="mx-auto text-white font-semibold py-2 text-[20px] ">
               Chuyển tiền
             </Text>
-          </View>
+          </TouchableOpacity>
 
         </ScrollView>
         <StatusBar backgroundColor="#fff" style="inverted" />
@@ -120,5 +176,14 @@ const ConfirmSend = () => {
     </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  input: {
+    height: 60,
+    margin: 12,
+    padding: 10,
+    fontSize:50,
+  }})
+
+
 
 export default ConfirmSend;
--- Client/ReactNative/components/Item_User.jsx
+++ Client/ReactNative/components/Item_User.jsx
@@ -1,8 +1,9 @@
 import { View, Text, TouchableOpacity,Image } from "react-native";
 import { router } from "expo-router";
 export default function ItemUser({item}){
+    const receiver = "667ec3fcc94a4a07d51ea301"
     return(
-        <TouchableOpacity onPress={()=>{router.push('home/confirm-send')}} className="flex-row items-centerp my-1 m-2 bg-white">
+        <TouchableOpacity onPress={()=>{router.push({pathname:'home/confirm-send',params: { receiver }})}} className="flex-row items-centerp my-1 m-2 bg-white">
             <Image className="w-[50px] h-[50px] rounded-full"
             source={{
                 uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNFPx_5UeN24Si_c-S11x4Xq0T10ydstaLig&s',
