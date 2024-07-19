12a09513775aad1c8a1bfd209dd32911b0f6a618
Luong Tu Nhan <tunhan281204@gmail.com>
2024-07-19 22:59:42 +0700

    feat: invoice

---
+++ Client/ReactNative/app/invoice/bs-security-code.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BsSecurityCode = () => {
+  return (
+    <View>
+      <Text>BsSecurityCode</Text>
+    </View>
+  )
+}
+
+export default BsSecurityCode
