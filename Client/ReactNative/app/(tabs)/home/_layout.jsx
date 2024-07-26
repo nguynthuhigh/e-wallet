96af1b61d246e12788152b48d681ce02e138227e
Luong Tu Nhan <tunhan281204@gmail.com>
2024-07-27 04:17:38 +0700

    feat: carousel credit card

--- Client/ReactNative/app/(tabs)/home/_layout.jsx
+++ Client/ReactNative/app/(tabs)/home/_layout.jsx
@@ -1,45 +1,49 @@
 import { Stack } from "expo-router";
 import { TouchableOpacity } from "react-native";
-import { useNavigation } from "expo-router";
+import { router } from "expo-router";
 import { LinearGradient } from "expo-linear-gradient";
-
+import BackArrow from "../../../assets/svg/arrow_back.svg";
 const StackLayout = () => {
-const navigation = useNavigation();
-
   return (
     <>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="confirm-send"
-          options={{
-            headerShown:false,
-
-            headerLeft: () => (
-              <>
-
-              </>
-            ),
-          }}
-        />
+        <Stack.Screen name="confirm-send" />
         <Stack.Screen
           name="receive-money"
           options={{
             headerShown: false,
           }}
         />
-        <Stack.Screen name="qr-payment" options={{ headerShown: false }} />
-        <Stack.Screen name="scan-qr" options={{ headerShown: false }} />
         <Stack.Screen name="transfer" options={{ headerShown: false }} />
         <Stack.Screen name="search-result" options={{ headerShown: false }} />
         <Stack.Screen name="confirm-bill" options={{ headerShown: false }} />
         <Stack.Screen name="details-bill" options={{ headerShown: false }} />
-        <Stack.Screen name="details-currency" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="details-currency"
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="list-currencies" options={{ headerShown: false }} />
 
         <Stack.Screen
-          name="deposit-withdrawl"
-          options={{ headerShown: false }}
+          name="deposit-withdraw"
+          options={{
+            headerTitle: "Nạp/Rút",
+            headerTintColor: "white",
+            headerBackground: () => (
+              <LinearGradient
+                style={{ flex: 1 }}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 0, y: 1 }}
+                colors={["#0094FF", "#F2F2F2"]}
+              />
+            ),
+            headerLeft: () => (
+              <TouchableOpacity onPress={() => router.push("/home")}>
+                <BackArrow width={24} height={24} />
+              </TouchableOpacity>
+            ),
+          }}
         />
       </Stack>
     </>
--- Client/ReactNative/app/(tabs)/wallet/_layout.jsx
+++ Client/ReactNative/app/(tabs)/wallet/_layout.jsx
@@ -8,7 +8,6 @@ const StackLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="notification" options={{ headerShown: false }} />
       <Stack.Screen
         name="credit_card"
         options={{
@@ -33,6 +32,26 @@ const StackLayout = () => {
         name="credit-card-linking"
         options={{
           headerTitle: "Liên kết thẻ tín dụng",
+          headerTintColor: "white",
+          headerBackground: () => (
+            <LinearGradient
+              style={{ flex: 1 }}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 0, y: 1 }}
+              colors={["#0094FF", "#F2F2F2"]}
+            />
+          ),
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => router.back()}>
+              <BackArrow width={24} height={24} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="credit-card-details"
+        options={{
+          headerTitle: "Chi tiết thẻ tín dụng",
           headerTintColor: "white",
           headerBackground: () => (
             <LinearGradient
